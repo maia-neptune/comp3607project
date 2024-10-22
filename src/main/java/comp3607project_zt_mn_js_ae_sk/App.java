419fcb6ed765ed54f91d6a8c2fbc1a9eb2cbf90f
Maia <maianeptune@gmail.colm>
2024-10-22 18:01:32 -0400

    first commit

---
+++ src/main/java/comp3607project_zt_mn_js_ae_sk/App.java
@@ -0,0 +1,13 @@
+package comp3607project_zt_mn_js_ae_sk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/comp3607project_zt_mn_js_ae_sk/AppTest.java
@@ -0,0 +1,20 @@
+package comp3607project_zt_mn_js_ae_sk;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
